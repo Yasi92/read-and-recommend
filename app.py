a2446666568bc138baf416b848b4677ed4666342
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-11-27 23:44:23 +0100

    Add login.html and register.html, add login and register functionality

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from flask import (
      request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 import pymongo
+from classes import RegisterForm, LoginForm, ReviewForm
+
 if os.path.exists("env.py"):
     import env
 
@@ -24,6 +27,36 @@ def get_books():
     books = mongo.db.books.find()
     categories = mongo.db.categories.find()
     return render_template("home.html", books=books, categories=categories)
+
+
+@app.route("/login", methods=["GET", "POST"])   
+def login():
+    login_form = LoginForm(request.form)
+
+    if request.method == "POST":
+        # Check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username" : login_form.username.data.lower()})
+
+        if existing_user:
+            # Check hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], login_form.password.data):
+                    session["user"] = login_form.username.data.lower()
+                    flash(f"Welcome {login_form.username.data}")
+                    return redirect(url_for('profile', username=session["user"]))
+            else:
+                # invalid password match
+                flash(f"Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        
+        else:
+            #username does not exist
+            flash(f"Incorrect Username and/or Password")
+            redirect(url_for("login"))
+
+    return render_template("login.html", login_form=login_form)
+
 
 
 if __name__ == "__main__":
---
+++ classes.py
@@ -0,0 +1,25 @@
+
+from wtforms import Form, StringField, PasswordField, validators, RadioField, TextAreaField
+from wtforms.fields.simple import TextAreaField
+from wtforms.widgets import TextArea
+import datetime
+
+# The authentication and registeration method has been learned from this tutorial on
+# https://www.youtube.com/watch?v=addnlzdSQs4 and wtforms documentation.
+class RegisterForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25),
+                            validators.DataRequired(), validators.Regexp('^\w+$',
+                            message="Username must contain only letters numbers or underscore")])
+    location = StringField('Your Location', [validators.Length(min=3, max=50), validators.DataRequired()])
+    email = StringField('Email', [validators.Length(min=6, max=50), validators.DataRequired()])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+class LoginForm(Form):
+    username = StringField('Username')
+    password = PasswordField('Password')
