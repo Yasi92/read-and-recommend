0cb56b5c6665ef4f56e5affc7bee9a916e637f03
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-12-08 17:07:20 +0100

    Change custome modal for delete message confirmation to a modal from materialize

--- static/js/script.js
+++ static/js/script.js
@@ -115,13 +115,6 @@ $(".loadMoreReview").on("click", function(){
 
 
 
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+$('.modal').modal();
 
 });
