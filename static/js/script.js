0ae2f5e1adee14be59b0dbac400a2b1ed6180f9a
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-12-15 20:09:50 +0100

    Change the history.go arg to test its functionality on ios devices

--- static/js/script.js
+++ static/js/script.js
@@ -106,7 +106,7 @@ $(".loadMoreReview").on("click", function(){
 
 // The history back button is learned form https://css-tricks.com/snippets/javascript/go-back-button/ and w3school 
 $(".back-btn").on("click", function(){
-  window.history.go(-1);
+  window.history.go(0);
 
 })
 
