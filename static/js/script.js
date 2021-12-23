fe88a4845819d83bf45fafc1512809cca9710a21
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-12-23 14:54:04 +0100

    Add docstrings to javascript file

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-
+  // It toggles the book cards and fade them into the window on page load.
   const cards = document.querySelectorAll('.book-card');
 
   cards.forEach((card) => {
@@ -10,15 +10,14 @@ $(document).ready(function () {
   });
 
 
+  // Sets the side nav to open from the right side of the window
   $('.sidenav').sidenav({
     edge: "right"
   });
 
 
-
+  // Initialize the form select in materialize
   $('select').formSelect();
-
-
 
 
   // This makes the entire div of book cards clickable.
@@ -27,7 +26,6 @@ $(document).ready(function () {
     window.location = $(this).find("a").attr("href");
     return false;
   });
-
 
 
   // This method has been borrowed from (https://www.freakyjolly.com/custom-jquery-function-read-more-and-read-less/)
@@ -43,7 +41,6 @@ $(document).ready(function () {
 
     //Traverse all selectors with this class and manupulate HTML part to show Read More
     $(".addReadMore").each(function () {
-
       var allstr = $(this).text();
       if (allstr.length > carLmt) {
         var firstSet = allstr.substring(0, carLmt);
@@ -54,8 +51,9 @@ $(document).ready(function () {
           readLessTxt + "</span>";
         $(this).html(strtoadd);
       }
-
     });
+
+
     //Read More and Read Less Click Event binding
     $(document).on("click", ".readMore,.readLess", function () {
       $(this).closest(".addReadMore").toggleClass("showlesscontent showmorecontent");
@@ -63,57 +61,33 @@ $(document).ready(function () {
   }
 
 
+  //Calling function after Page Load
   $(function () {
-    //Calling function after Page Load
     AddReadMore();
   });
 
 
-
-  // This function display books added by user on profile page in a max number and loads more books on click  
+  // This function display books added by user on profile page in limited numbers and loads more books on click  
+  // It slices the books added by the user on the profile page
   $(".collection-item").slice(0, 6).show();
 
+
+  // It displays the loadmore buttton if there are hidden books in the list
   if ($(".collection-item:hidden").length != 0) {
     $(".loadMoreBooks").show();
   }
 
+  // It displays more books on click on the loadMore button
   $(".loadMoreBooks").on("click", function () {
     $(".collection-item:hidden").slice(0, 6).show();
 
     if ($(".collection-item:hidden").length == 0) {
       $(".loadMoreBooks").fadeOut();
     }
-
   })
 
 
-
-  $('.modal').modal();
-
-  var date = new Date().getFullYear();
-  document.getElementById("year").innerHTML = date;
-
-
-
-  // The history back button is learned form https://css-tricks.com/snippets/javascript/go-back-button/ and w3school 
-  $(".back-btn").on("click", function (e) {
-    e.preventDefault();
-    window.history.go(-1);
-    return false;
-
-  })
-
-
-  // Get the height of header and footer on different screen size to push 
-  // the footer to the bottom of the page regardless of the size of content.
-  // The method has been learned from (https://www.youtube.com/watch?v=US_3XvufMLU) and manipulated by me to make it responsive to all screen sizes.
-  var header = document.querySelector("header").offsetHeight;
-  var footer = document.querySelector("footer").offsetHeight;
-
-  document.getElementById("main").style.minHeight = "calc( 100vh - " + header + "px" + " - " + footer + "px )";
-
-
-  // This function display reviews added by user on profile page in a max number and loads more reviews on click  
+  // This function display reviews added by user on profile page in a limited numbers and loads more reviews on click  
   $(".collection-item-review").slice(0, 4).show();
 
 
@@ -132,13 +106,50 @@ $(document).ready(function () {
 
 
 
+  // Calls the modals
+  $('.modal').modal();
 
+
+  // Gets the year we are in, to display in the footer
+  var date = new Date().getFullYear();
+  document.getElementById("year").innerHTML = date;
+
+
+  // The history back button is learned form https://css-tricks.com/snippets/javascript/go-back-button/ and w3school 
+  $(".back-btn").on("click", function (e) {
+    e.preventDefault();
+    window.history.go(-1);
+    return false;
+
+  })
+
+
+  /* The method has been learned from this tutorial (https://www.youtube.com/watch?v=US_3XvufMLU) and
+   manipulated by me to make it responsive to all screen sizes.*/
+
+  /* Get the height of header and footer on different screen size to push 
+    the footer to the bottom of the page regardless of the size of content.*/
+  var header = document.querySelector("header").offsetHeight;
+  var footer = document.querySelector("footer").offsetHeight;
+
+  document.getElementById("main").style.minHeight = "calc( 100vh - " + header + "px" + " - " + footer + "px )";
+
+
+  // This fixes the position of the back-to-top button on top of the footer
+  document.getElementById("myBtn").style.bottom = footer + "px ";
+
+
+  
+
+  /* The back to top button is learned from this thread on stackoverflow
+   (https://stackoverflow.com/questions/14249998/jquery-back-to-top)*/
 
   //check if window scroll is < 100
   if ($(window).scrollTop() < 100) {
     $('#myBtn').hide();
   }
 
+  // Checks if the window scroll is more than 500 and then displays the button
   $("body").on("scroll", function () {
     if ($(this).scrollTop() > 500) {
       $('#myBtn').fadeIn();
@@ -147,6 +158,7 @@ $(document).ready(function () {
     }
   });
 
+  // Scrolls up to the top of the page on click
   $("#myBtn").click(function () {
     $("html, body").animate({
       scrollTop: 0
@@ -155,19 +167,11 @@ $(document).ready(function () {
   })
 
 
-  // Fixing the back-to-top button on top of the footer
-  document.getElementById("myBtn").style.bottom = footer + "px ";
-
-
-
-
+  // It hides the back button on the books.html if the previous url is add_book
+  // And adds margin-top to the row
   if (document.referrer.includes("add_book")) {
     $(".back-btn").hide();
     $(".bottom-bordered-row").css("margin-top", "3rem");
   }
-
-
-
-
 
 })
