7c2e40b68df230eea210ed9c515f5fbf050bdd5c
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-12-24 21:26:57 +0100

    Tidy up the classes.py

--- classes.py
+++ classes.py
@@ -1,10 +1,8 @@
 
-from wtforms import Form, StringField, PasswordField, validators, TextAreaField, EmailField, BooleanField
+from wtforms import (Form, StringField, PasswordField,
+                    validators, TextAreaField, EmailField,
+                    BooleanField)
 from wtforms.fields.choices import SelectField
-from wtforms.widgets import TextArea
-import datetime
-
-
 
 # The authentication and registeration method has been learned from this tutorial on
 # https://www.youtube.com/watch?v=addnlzdSQs4 and wtforms documentation.
@@ -12,12 +10,15 @@ class RegisterForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=25),
                             validators.DataRequired(), validators.Regexp('^\w+$',
                             message="Username must contain only letters numbers or underscore")])
-    location = StringField('Your Location', [validators.Length(min=3, max=50), validators.DataRequired()])
-    email = EmailField('Email', [validators.Length(min=6, max=50), validators.DataRequired()])
+    location = StringField('Your Location',
+                             [validators.Length(min=3, max=50),
+                             validators.DataRequired()])
+    email = EmailField('Email',
+                    [validators.Length(min=6, max=50),
+                    validators.DataRequired()])
     password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords do not match')
-
     ])
     confirm = PasswordField('Confirm Password')
 
@@ -29,14 +30,17 @@ class LoginForm(Form):
 
 
 class ReviewForm(Form):
-    review =  TextAreaField('Write Here', [validators.Length(min=3, max=8000), validators.DataRequired()], default=None)
+    review =  TextAreaField('Write Here',
+                            [validators.Length(min=3, max=8000),
+                            validators.DataRequired()], default=None)
     
 
-
-
 class EditProfile(Form):
-    location = StringField('Your Location', [validators.Length(min=3, max=50), validators.DataRequired()])
-    email = EmailField('Email', [validators.Length(min=6, max=50), validators.DataRequired()])
+    location = StringField('Your Location',
+                        [validators.Length(min=3, max=50),
+                        validators.DataRequired()])
+    email = EmailField('Email', [validators.Length(min=6, max=50),
+                         validators.DataRequired()])
     username = StringField('Username', [validators.Length(min=4, max=25),
                             validators.DataRequired(), validators.Regexp('^\w+$',
                             message="Username must contain only letters numbers or underscore")])
@@ -47,15 +51,22 @@ class AddBook(Form):
     categories= [('1', 'literature'), ('2', 'romance'),
                 ('3', 'art'), ('4', 'biography'), ('5', 'business'),
                 ('6', 'children'), ('7', 'history'), ('8', 'nonfiction')]
-    title = StringField('Title', [validators.Length(min=3, max=50), validators.DataRequired()])
-    author = StringField('Author', [validators.Length(min=3, max=50), validators.DataRequired()])
+    title = StringField('Title', [validators.Length(min=3, max=50),
+                         validators.DataRequired()])
+    author = StringField('Author', [validators.Length(min=3, max=50),
+                         validators.DataRequired()])
     category = SelectField('', choices=categories)
-    publisher = StringField('Publisher', [validators.Length(min=3, max=50), validators.DataRequired()])
+    publisher = StringField('Publisher', [validators.Length(min=3, max=50),
+                             validators.DataRequired()])
     pages = StringField('Pages', [validators.DataRequired()])
     language = StringField('Language', [validators.DataRequired()])
-    shopping_link = StringField('shopping link', [validators.Length(min=3, max=350), validators.DataRequired()])
-    image = StringField('Image url', [validators.Length(min=3, max=350), validators.DataRequired()])
-    desc = TextAreaField('Description', [validators.Length(min=3, max=8000), validators.DataRequired()], default=None) 
+    shopping_link = StringField('shopping link',
+                            [validators.Length(min=3, max=350), 
+                            validators.DataRequired()])
+    image = StringField('Image url', [validators.Length(min=3, max=350),
+                         validators.DataRequired()])
+    desc = TextAreaField('Description', [validators.Length(min=3, max=8000),
+                         validators.DataRequired()], default=None) 
     best_seller= BooleanField('')
     price = StringField('Price', [validators.DataRequired()])
 
